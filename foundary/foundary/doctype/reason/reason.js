675c6f708d206da3bf5490ac88902b6347b9f354
FinByz <info@finbyz.tech>
2024-02-02 17:37:06 +0530

    Added new child table rejections in Job Card

---
+++ foundary/foundary/doctype/reason/reason.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Finbyz Tech PVT LTD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Reason', {
+	// refresh: function(frm) {
+
+	// }
+});
--- foundary/public/doc_js/job_card.js
+++ foundary/public/doc_js/job_card.js
@@ -4,6 +4,15 @@ frappe.ui.form.on('Job Card',  {
         var child = frm.add_child("scrap_items");
         child.item_code = frm.doc.production_item;
         child.item_name = frm.doc.item_name;
-        child.stock_qty = frm.doc.process_loss_qty;
+        child.stock_qty = frm.doc.process_loss_qty; 
+    },
+    rejections_add: function(frm) {
+
+    }
+});
+frappe.ui.form.on('Job Card Rejections',  {
+    rejections_add: function(frm,cdt,cdn) {
+        frappe.model.set_value(cdt, cdn, 'qty', frm.doc.process_loss_qty);
+        frm.refresh_field('rejections');
     }
 });
