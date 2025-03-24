c437d05215eb5b97728b569d049532dfcbf22dfd
finbyz <info@finbyz.com>
2025-03-24 13:31:20 +0530

    changes in scrap item code

--- foundary/foundary/doc_events/stock_entry.py
+++ foundary/foundary/doc_events/stock_entry.py
@@ -4,10 +4,12 @@ from foundary.foundary.doc_events.work_order import update_work_order_pending_fi
 from frappe.query_builder.functions import Sum
 from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 from frappe.utils import (cint,flt)
-
+import erpnext
+from erpnext.stock.stock_ledger import get_valuation_rate
 
 def validate(self,method):
-	update_additional_cost(self)
+	# update_additional_cost(self)
+	pass
 
 def on_submit(self, method):
 	if self.work_order:
@@ -55,32 +57,14 @@ class FinishedGoodError(frappe.ValidationError):
 
 class CustomStockEntry(StockEntry):
 	def before_validate(self):
-
 		if self.stock_entry_type == "Manufacture" and self.bom_no:
 			self.update_valuation_of_scrap_quantity()
 	
 	def update_valuation_of_scrap_quantity(self):
-		
-		total_outgoing_value = self.total_outgoing_value
-		scrap_item_value = 0
 		finished_item = frappe.db.get_value("BOM", self.bom_no, "item")
-		finished_item_qty = 0
-
 		for row in self.items:
-			if row.is_scrap_item and row.item_code != finished_item:
-				scrap_item_value += row.amount
-			
-			if row.item_code == finished_item:
-				finished_item_qty += row.qty
-		
-		net_outgoing = total_outgoing_value - scrap_item_value
-		rate = net_outgoing / finished_item_qty
-
-		for row in self.items:
-			if row.item_code == finished_item:
-				if row.item_code and row.is_scrap_item:
-					row.allow_zero_valuation_rate = 0
-					row.basic_rate = rate
+			if row.item_code == finished_item and row.item_code and row.is_scrap_item:
+				row.allow_zero_valuation_rate = 0
 		
 		self.calculate_rate_and_amount()
 
@@ -207,15 +191,127 @@ class CustomStockEntry(StockEntry):
 
 			return scrap_items
 	
-	def get_basic_rate_for_manufactured_item(self, finished_item_qty, outgoing_items_cost=0) -> float:
-		scrap_items_cost = sum([flt(d.basic_amount) for d in self.get("items") if d.is_scrap_item])
+	def set_basic_rate(self, reset_outgoing_rate=True, raise_error_if_no_rate=True):
+		"""
+		Set rate for outgoing, scrapped and finished items
+		"""
+		# Set rate for outgoing items
+		finished_item = frappe.db.get_value("BOM", self.bom_no, "item")
+		outgoing_items_cost = self.set_rate_for_outgoing_items(reset_outgoing_rate, raise_error_if_no_rate)
+		finished_item_qty = sum(d.transfer_qty for d in self.items if (d.is_finished_item or (d.is_scrap_item and  d.item_code == finished_item)))
 
-		# Get raw materials cost from BOM if multiple material consumption entries
-		if not outgoing_items_cost and frappe.db.get_single_value(
-			"Manufacturing Settings", "material_consumption", cache=True
-		):
-			bom_items = self.get_bom_raw_materials(finished_item_qty)
-			outgoing_items_cost = sum([flt(row.qty) * flt(row.rate) for row in bom_items.values()])
-		if finished_item_qty <= 0:
-			frappe.throw("Please recheck the finish quantity or process loss quantity. There is no finish quantity left after considering the process loss quantity.")
+		items = []
+		# Set basic rate for incoming items
+		for d in self.get("items"):
+			if d.s_warehouse or d.set_basic_rate_manually:
+				continue
+
+			if d.allow_zero_valuation_rate:
+				d.basic_rate = 0.0
+				items.append(d.item_code)
+
+			elif d.is_finished_item or (d.is_scrap_item and d.item_code == finished_item):
+				if self.purpose == "Manufacture":
+					d.basic_rate = self.get_basic_rate_for_manufactured_item(
+						finished_item_qty, outgoing_items_cost
+					)
+				elif self.purpose == "Repack":
+					d.basic_rate = self.get_basic_rate_for_repacked_items(d.transfer_qty, outgoing_items_cost)
+
+			if not d.basic_rate and not d.allow_zero_valuation_rate:
+				if self.is_new():
+					raise_error_if_no_rate = False
+
+				d.basic_rate = get_valuation_rate(
+					d.item_code,
+					d.t_warehouse,
+					self.doctype,
+					self.name,
+					d.allow_zero_valuation_rate,
+					currency=erpnext.get_company_currency(self.company),
+					company=self.company,
+					raise_error_if_no_rate=raise_error_if_no_rate,
+					batch_no=d.batch_no,
+					serial_and_batch_bundle=d.serial_and_batch_bundle,
+				)
+
+			# do not round off basic rate to avoid precision loss
+			d.basic_rate = flt(d.basic_rate)
+			d.basic_amount = flt(flt(d.transfer_qty) * flt(d.basic_rate), d.precision("basic_amount"))
+
+		if items:
+			message = ""
+
+			if len(items) > 1:
+				message = _(
+					"Items rate has been updated to zero as Allow Zero Valuation Rate is checked for the following items: {0}"
+				).format(", ".join(frappe.bold(item) for item in items))
+			else:
+				message = _(
+					"Item rate has been updated to zero as Allow Zero Valuation Rate is checked for item {0}"
+				).format(frappe.bold(items[0]))
+
+			frappe.msgprint(message, alert=True)
+	
+	def get_basic_rate_for_manufactured_item(self, finished_item_qty, outgoing_items_cost=0) -> float:
+		finished_item = frappe.db.get_value("BOM", self.bom_no, "item")
+		settings = frappe.get_single("Manufacturing Settings")
+		scrap_items_cost = sum([flt(d.basic_amount) for d in self.get("items") if d.is_scrap_item and d.item_code != finished_item])
+
+		if settings.material_consumption:
+			if settings.get_rm_cost_from_consumption_entry and self.work_order:
+				# Validate only if Material Consumption Entry exists for the Work Order.
+				if frappe.db.exists(
+					"Stock Entry",
+					{
+						"docstatus": 1,
+						"work_order": self.work_order,
+						"purpose": "Material Consumption for Manufacture",
+					},
+				):
+					for item in self.items:
+						if not item.is_finished_item and not item.is_scrap_item:
+							label = frappe.get_meta(settings.doctype).get_label(
+								"get_rm_cost_from_consumption_entry"
+							)
+							frappe.throw(
+								_(
+									"Row {0}: As {1} is enabled, raw materials cannot be added to {2} entry. Use {3} entry to consume raw materials."
+								).format(
+									item.idx,
+									frappe.bold(label),
+									frappe.bold(_("Manufacture")),
+									frappe.bold(_("Material Consumption for Manufacture")),
+								)
+							)
+
+					if frappe.db.exists(
+						"Stock Entry",
+						{"docstatus": 1, "work_order": self.work_order, "purpose": "Manufacture"},
+					):
+						frappe.throw(
+							_("Only one {0} entry can be created against the Work Order {1}").format(
+								frappe.bold(_("Manufacture")), frappe.bold(self.work_order)
+							)
+						)
+
+					SE = frappe.qb.DocType("Stock Entry")
+					SE_ITEM = frappe.qb.DocType("Stock Entry Detail")
+
+					outgoing_items_cost = (
+						frappe.qb.from_(SE)
+						.left_join(SE_ITEM)
+						.on(SE.name == SE_ITEM.parent)
+						.select(Sum(SE_ITEM.valuation_rate * SE_ITEM.transfer_qty))
+						.where(
+							(SE.docstatus == 1)
+							& (SE.work_order == self.work_order)
+							& (SE.purpose == "Material Consumption for Manufacture")
+						)
+					).run()[0][0] or 0
+
+			elif not outgoing_items_cost:
+				bom_items = self.get_bom_raw_materials(finished_item_qty)
+				outgoing_items_cost = sum([flt(row.qty) * flt(row.rate) for row in bom_items.values()])
+
 		return flt((outgoing_items_cost - scrap_items_cost) / finished_item_qty)
