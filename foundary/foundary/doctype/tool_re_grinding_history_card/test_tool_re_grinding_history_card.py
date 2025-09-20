22bde70eb2c94f70dd114c13008c3d6c4cbd4aee
Finbyz <info@finbyz.tech>
2025-09-20 05:24:08 +0000

    tool changes DONE NB

--- foundary/foundary/doctype/tool_re_grinding_history_card/test_tool_re_grinding_history_card.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Finbyz Tech PVT LTD and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestToolReGrindingHistoryCard(FrappeTestCase):
-	pass
